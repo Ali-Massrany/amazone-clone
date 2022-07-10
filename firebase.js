43fc0a7e4330229bfaef15445e97425dce89c10f
Ali-Massrany <alimassrany453099@gmail.com>
2022-07-10 20:13:47 +0300

    redeploy 6

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCLCrA0gB69ZHI2NXVpXQOlCpaikwGtwNM",
-  authDomain: "fir-c0ae5.firebaseapp.com",
-  projectId: "fir-c0ae5",
-  storageBucket: "fir-c0ae5.appspot.com",
-  messagingSenderId: "484642405726",
-  appId: "1:484642405726:web:68c6463708bd8dfa4a23cf",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
